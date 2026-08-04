48b8705c44913c4845d00ffb43097ad77a2810ae
Lucas Martin Schlez <lucasschlez@gmail.com>
2026-08-04 12:21:18 -0300

    fix categorias

--- constants/categories.ts
+++ constants/categories.ts
@@ -2,7 +2,7 @@ export const CATEGORIES = {
   'Política':       ['Gobierno Nacional', 'Justicia', 'Elecciones', 'Educación', 'Seguridad'],
   'Economía':       ['Dólar y Mercados', 'Inflación y Consumo', 'Empresas y Negocios', 'Inversiones', 'Emprendedores'],
   'Internacional':  ['EEUU', 'Medio Oriente', 'Europa', 'América Latina', 'Conflictos', 'Geopolítica'],
-  'Deportes':       ['Fútbol', 'Mundial 2026', 'Básquet', 'Tenis', 'Rugby'],
+  'Deportes':       ['Fútbol', 'F1', 'Básquet', 'Tenis', 'Rugby'],
   'Sociedad':       ['Salud', 'Bienestar', 'Clima y Ambiente', 'Historias Humanas', 'Tendencias Y Vida'],
   'Tecnología':     ['Inteligencia Artificial', 'Ciencia y Espacio', 'Apps y Redes', 'Innovación', 'Videojuegos'],
   'Entretenimiento/Cultura': ['Cine y Series', 'Música', 'Turismo y Viajes', 'Streaming', 'Autos', 'Viral y Trending','Teatro y Literatura'],
